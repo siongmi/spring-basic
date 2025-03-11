7a5f4212f0f9c6c4a46002b43dcac11283eb8661
siongmi <tldmseeo@naver.com>
2025-03-11 13:09:57 +0900

    feat. 빈네임기반매핑 예시

---
+++ src/main/java/org/codenova/spring/controller/IndexHandler.java
@@ -0,0 +1,15 @@
+package org.codenova.spring.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.HttpRequestHandler;
+
+import java.io.IOException;
+
+public class IndexHandler implements HttpRequestHandler {
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("IndexHandler.handleRequest");
+    }
+}
