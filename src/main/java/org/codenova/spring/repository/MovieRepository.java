5f7f14e6e527aa50968653882f5ac6904c3c3aab
siongmi <tldmseeo@naver.com>
2025-03-14 14:31:11 +0900

    feat. movie repository 초기화

--- src/main/java/org/codenova/spring/repository/MovieRepository.java
+++ src/main/java/org/codenova/spring/repository/MovieRepository.java
@@ -19,22 +19,6 @@ public class MovieRepository {
 
     public boolean create(Movie movie){
         boolean result = false;
-        try{
-            Connection conn = dataSource.getConnection();
-
-            PreparedStatement ps = conn.prepareStatement("insert into movies values(null,?,?,?,?)");
-            ps.setString(1, movie.getTitle());
-            ps.setString(2,movie.getDescription());
-            ps.setString(3, movie.getGenre());
-            ps.setDate(4, Date.valueOf(movie.getReleaseDate()));
-
-            ps.executeUpdate();
-
-            result = true;
-        }catch (Exception e){
-            System.out.println("MovieRepository.create error: "+e);
-        }
-
 
         return result;
     }
